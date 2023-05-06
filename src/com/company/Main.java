75a652620bc92f658edb2601f68a0fc55c8cc023
boboyevnurbek <boboyevnurbek95@gmail.com>
2023-05-06 18:11:54 +0500

    esfsfsf

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello from java");
         System.out.println("Hello from java");
         System.out.println("Hello from java");
+        System.out.println("Hello from java");
     }
 }
