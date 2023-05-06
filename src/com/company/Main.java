aad2dfa12f971caa8cce17b8ecfa021f48c723d2
boboyevnurbek <boboyevnurbek95@gmail.com>
2023-05-06 18:19:05 +0500

    aaaaaaaaaaaaaa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from java");
-        System.out.println("Hello from javascript");
+        System.out.println("Hello from javascript2");
         System.out.println("Hello from python");
-        System.out.println("Hello from java");
+        System.out.println("Hello from java4");
+        System.out.println("Hello from ");
     }
 }
